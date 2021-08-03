9088eec751b04f30109fb5c92a7dca245dc902ea
connect.basillabs@gmail.com <suki22889@gmail.com>
2021-08-02 22:15:24 -0400

    Changes

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
-var connection = mysql.createPool({
+const connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
+});
+
+// open the MySQL connection
+connection.getConnection(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
 });
 
 module.exports = connection;
